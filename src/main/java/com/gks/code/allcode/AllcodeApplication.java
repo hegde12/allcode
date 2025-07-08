525a4282cf0f106b1b37a5e943efbc0e52969846
hegde12 <hegdegireesh012@gmail.com>
2025-07-08 18:49:43 +0530

    Initialize project structure with Maven, add Spring Boot dependencies, and create application files

---
+++ src/main/java/com/gks/code/allcode/AllcodeApplication.java
@@ -0,0 +1,13 @@
+package com.gks.code.allcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllcodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllcodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gks/code/allcode/AllcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gks.code.allcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllcodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
